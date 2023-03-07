4d08a154365d02aa100d201522789774508ccfe7
Oduse David <89360467+KingDavidJnr@users.noreply.github.com>
2023-03-07 02:23:04 +0000

    added constants.js

---
+++ constants/constants.js
@@ -0,0 +1,28 @@
+const constants = {
+    MESSAGES: {
+        CONNECTED: "Connection Successful",
+        ERROR: "There was an error",
+        USER: {
+            FETCHED: "User(s) fetched successfully!",
+            UPDATED: "User details updated successfully!",
+            CREATED: "New User created successfully!",
+            DELETED: "User deleted successfully!"
+        },
+
+        POSTS: {
+            FETCHED: "Post-it fetched successfully!",
+            UPDATED: "Post-it updated successfully!",
+            CREATED: "Your Post-it has been created successfully!",
+            DELETED: "Post-it deleted successfully!"
+        },
+        
+        COMMENTS: {
+            FETCHED: "Comment(s) fetched successfully!",
+            UPDATED: "Comment has been successfully updated!",
+            CREATED: "Comment was posted successfully!",
+            DELETED: "Comment successfully deleted"
+        }
+    }
+}
+
+module.exports = constants;
